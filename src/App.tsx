4aa3a091b95555afa7bb23a41e6409a92bbd728c
MikeC137 <michalischatzinikolas13@gmail.com>
2025-11-19 12:05:04 -0500

    feat: add routing and new pages for Homepage, Dashboard, and History

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Dashboard from "./pages/Dashboard";
+import History from "./pages/History";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  );
 }
 
 export default App;
