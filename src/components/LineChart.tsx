c48871d3096f70d17b6ea214541f57def74f501f
MikeC137 <michalischatzinikolas13@gmail.com>
2025-12-29 23:44:23 -0500

    refactor: update SpendingChart styling with border and margin adjustments for enhanced visual appeal

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -64,7 +64,7 @@ function SpendingChart() {
   }
 
   return (
-    <div className="w-full max-w-sm md:max-w-md lg:max-w-lg mx-auto md:mx-0">
+    <div className="w-full border border-radius-1 py-4 rounded-xl border-zinc-700 px-4 max-w-sm md:max-w-md lg:max-w-lg mx-auto md:mx-0">
       <div className="flex items-center mb-4">
         <h3 className="text-zinc-300 text-lg font-semibold mx-4">
           Spending Over Time
@@ -76,7 +76,10 @@ function SpendingChart() {
         />
       </div>
       <ResponsiveContainer width="100%" height={300}>
-        <LineChart data={filledChartData}>
+        <LineChart
+          data={filledChartData}
+          margin={{ top: 5, right: 20, bottom: 20, left: 5 }}
+        >
           <CartesianGrid strokeDasharray="3 3" stroke="#3f3f46" />
           <XAxis
             dataKey="name"
